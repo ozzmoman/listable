ebaf87719571a47b3e1655eb05e62ed5ceed30d1
rezaw <oswald.cobolpot@gmail.com>
2023-11-03 02:37:40 +0330

    header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,31 @@
 import {FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <spam className='text-slate-500'>Reza</spam>
-            <spam className='text-slate-700'>Jobs</spam>
+            <spam className='text-slate-500'>Listable</spam>
+            <spam className='text-slate-700'>-Site</spam>
         </h1>
+        </Link>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
             <input type='text' placeholder='search...' 
             className='bg-transparent focus:outline-none w-24 sm:w-64' />
             <FaSearch className='text-slate-600' />
         </form>
         <ul className='flex gap-4' >
-          <li className='hidden sm:inLine text-slate-700 hover:underline' >Home</li>
-          <li className='hidden sm:inLine text-slate-700 hover:underline' >About</li>
-          <li className='hidden sm:inLine text-slate-700 hover:underline' >Sign in</li>
+          <Link to='/'>
+          <li className='sm:inLine text-slate-700 hover:underline'>Home</li>
+          </Link>
+          <Link to='/about'>
+          <li className='sm:inLine text-slate-700 hover:underline'>About</li>
+          </Link>
+          <Link to='/sign-in'>
+          <li className=' text-slate-700 hover:underline'>Sign in</li>
+          </Link>
           
         </ul>
        </div> 
